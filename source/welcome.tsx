e6ab38b79376541bc89f92884ff6b8e3f91d957d
Kowsika M <kkowsika07@gmail.com>
2025-05-22 10:24:36 +0530

    first commit

---
+++ source/welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import{View,Text} from 'react-native';
+ 
+const welcome= ()=>{
+    return (
+        <View>
+            <Text>Hello from REACTNATIVE</Text>
+        </View>
+    );
+};
+
+export default welcome;
